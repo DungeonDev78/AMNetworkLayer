2eb68849ad45ad2c609e46b6ba53acab774b4c5e
Alessandro Manilii <a.manilii@fastwebnet.it>
2021-03-22 11:39:54 +0100

    Fix

--- Sources/AMNetworkLayer/Core/AMNetError.swift
+++ Sources/AMNetworkLayer/Core/AMNetError.swift
@@ -19,8 +19,6 @@ public enum AMNetError: Error {
     case notFound
     case unauthorizedAccess
     case customUser(description: String, recovery: String = "", code: Int?)
-    
-    public init() { }
 }
 
 // MARK: - Error Messages
